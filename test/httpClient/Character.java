917c62c00b88b7bf52beb3b14b7ad026ff5fcbc7
Jesus Arnas Iñigo <jarnas@plainconcepts.com>
2019-12-10 09:53:13 +0100

    WIP: httpclient

---
+++ test/httpClient/Character.java
@@ -0,0 +1,19 @@
+package httpClient;
+
+public class Character {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Character [name=" + name + "]";
+	}
+}
